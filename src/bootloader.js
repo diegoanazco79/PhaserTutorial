7b3c1135431592ea788bd61484ff45e94b77de20
diegoanazco79 <djanazcob@gmail.com>
2021-04-07 18:26:19 -0500

    Pong Game: Insertando imagenes

--- src/bootloader.js
+++ src/bootloader.js
@@ -3,9 +3,18 @@ class Bootloader extends Phaser.Scene {
         super ({key: "Bootloader"})
     }
     preload(){
-        console.log("Se ha cargado Bootloader")
+        this.load.on("complete", () => {
+            this.scene.start("Scene_play") //Llamamos a la escena que queremos.
+        })
+
+        this.load.image("ball", "./assets/ball.png")
+        this.load.image("izquierda", "./assets/left_pallete.png")
+        this.load.image("derecha", "./assets/right_pallete.png")
+        this.load.image("separador", "./assets/separator.png")
+        
     }
     
 }
 
 export default Bootloader
+//Bootloader sirve para cargar todo lo necesario
