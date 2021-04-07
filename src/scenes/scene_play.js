c3bb9c1ce7e48ed32212ef7213a381c896ac0c9f
diegoanazco79 <djanazcob@gmail.com>
2021-04-07 18:36:31 -0500

    Pong Game: Colliders y bounces

--- src/scenes/scene_play.js
+++ src/scenes/scene_play.js
@@ -1,3 +1,4 @@
+import Palas from '../gameObjects/palas.js'
 class Scene_play extends Phaser.Scene{
     constructor(){
         super({key: "Scene_play"})
@@ -11,11 +12,23 @@ class Scene_play extends Phaser.Scene{
         this.add.image(center_width, center_height,"separador");
 
         //Palas
-        this.izquierda = this.add.image(30, center_height , "izquierda")
-        this.right = this.add.image(this.sys.game.config.width-30, center_height , "derecha")
+        this.izquierda = new Palas(this, 30, center_height, "izquierda")
+        this.right = new Palas(this, this.sys.game.config.width-30,  center_height, "derecha")
 
         //Bola
+        this.physics.world.setBoundsCollision(false, false, true, true)
         this.ball = this.physics.add.image(center_width, center_height, "ball")
+        this.ball.setCollideWorldBounds(true)
+        this.ball.setBounce(1)
+        this.ball.setVelocityX(-180)
+
+        //Físicas
+        this.physics.add.collider(this.ball, this.izquierda, this.chocaPala, null, this)
+        this.physics.add.collider(this.ball, this.derecha, this.chocaPala, null, this)
+    }
+
+    chocaPala(){
+
     }
 }
 
